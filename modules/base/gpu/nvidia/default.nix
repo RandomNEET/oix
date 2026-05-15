8be1ec8a5860944d35aef02765cc0fe081fde5bf
RandomNEET <dev@randomneet.me>
2026-05-15 12:00:13 +0800

    nvidia: use allowUnfreePackages instead

--- modules/base/gpu/nvidia/default.nix
+++ modules/base/gpu/nvidia/default.nix
@@ -2,65 +2,68 @@
   config,
   lib,
   pkgs,
+  meta,
   ...
 }:
 let
   nvidiaDriverChannel = config.boot.kernelPackages.nvidiaPackages.latest; # stable, latest, beta, etc.
 in
 {
-  config = lib.mkIf (config.base.gpu == "nvidia") {
-    environment.sessionVariables = lib.optionalAttrs config.desktop.enable {
-      NVD_BACKEND = "direct";
-      GBM_BACKEND = "nvidia-drm";
-      WLR_NO_HARDWARE_CURSORS = "1";
-      LIBVA_DRIVER_NAME = "nvidia";
-      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+  config = lib.mkIf (config.base.gpu == "nvidia") (
+    {
+      environment.sessionVariables = lib.optionalAttrs config.desktop.enable {
+        NVD_BACKEND = "direct";
+        GBM_BACKEND = "nvidia-drm";
+        WLR_NO_HARDWARE_CURSORS = "1";
+        LIBVA_DRIVER_NAME = "nvidia";
+        __GLX_VENDOR_LIBRARY_NAME = "nvidia";
 
-      __GL_GSYNC_ALLOWED = "1"; # GSync
-    };
-    # Load nvidia driver for Xorg and Wayland
-    services.xserver.videoDrivers = [ "nvidia" ]; # or "nvidiaLegacy470", etc.
-    boot.kernelParams = lib.optionals (lib.elem "nvidia" config.services.xserver.videoDrivers) [
-      "nvidia-drm.modeset=1"
-      "nvidia_drm.fbdev=1"
-      # Fixes sleep/suspend
-      "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
-      "nvidia.NVreg_TemporaryFilePath=/var/tmp"
-    ];
-    hardware = {
-      nvidia = {
-        open = true;
-        nvidiaSettings = false;
-        powerManagement.enable = true; # Fixes sleep/suspend
-        modesetting.enable = true; # Modesetting is required.
-        package = nvidiaDriverChannel;
+        __GL_GSYNC_ALLOWED = "1"; # GSync
       };
-      graphics = {
-        enable = true;
-        enable32Bit = true;
-        extraPackages = with pkgs; [
-          nvidia-vaapi-driver
-          libva-vdpau-driver
-          libvdpau-va-gl
+      # Load nvidia driver for Xorg and Wayland
+      services.xserver.videoDrivers = [ "nvidia" ]; # or "nvidiaLegacy470", etc.
+      boot.kernelParams = lib.optionals (lib.elem "nvidia" config.services.xserver.videoDrivers) [
+        "nvidia-drm.modeset=1"
+        "nvidia_drm.fbdev=1"
+        # Fixes sleep/suspend
+        "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
+        "nvidia.NVreg_TemporaryFilePath=/var/tmp"
+      ];
+      hardware = {
+        nvidia = {
+          open = true;
+          nvidiaSettings = false;
+          powerManagement.enable = true; # Fixes sleep/suspend
+          modesetting.enable = true; # Modesetting is required.
+          package = nvidiaDriverChannel;
+        };
+        graphics = {
+          enable = true;
+          enable32Bit = true;
+          extraPackages = with pkgs; [
+            nvidia-vaapi-driver
+            libva-vdpau-driver
+            libvdpau-va-gl
+          ];
+        };
+      };
+      nix.settings = {
+        substituters = [ "https://cuda-maintainers.cachix.org" ];
+        trusted-public-keys = [
+          "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
         ];
       };
-    };
-    nixpkgs.config = {
-      nvidia.acceptLicense = true;
-      allowUnfreePredicate =
-        pkg:
-        builtins.elem (lib.getName pkg) [
+    }
+    // lib.optionalAttrs (!meta.allowUnfree) {
+      nixpkgs.config = {
+        nvidia.acceptLicense = true;
+        allowUnfreePackages = [
           "cudatoolkit"
           "nvidia-persistenced"
           "nvidia-settings"
           "nvidia-x11"
         ];
-    };
-    nix.settings = {
-      substituters = [ "https://cuda-maintainers.cachix.org" ];
-      trusted-public-keys = [
-        "cuda-maintainers.cachix.org-1:0dq3bujKpuEPMCX6U4WylrUDZ9JyUG0VpVZa7CNfq5E="
-      ];
-    };
-  };
+      };
+    }
+  );
 }
