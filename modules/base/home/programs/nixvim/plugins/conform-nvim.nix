395413f3e5c5ea6e8932d56a88a245a3c00a8454
RandomNEET <dev@randomneet.me>
2026-06-14 20:46:35 +0800

    nixvim: update conform config

--- modules/base/home/programs/nixvim/plugins/conform-nvim.nix
+++ modules/base/home/programs/nixvim/plugins/conform-nvim.nix
@@ -1,9 +1,25 @@
-{ pkgs, ... }:
 {
   programs.nixvim = {
     plugins.conform-nvim = {
       enable = true;
+      autoInstall = {
+        enable = true;
+      };
+      luaConfig = {
+        pre = ''
+          local slow_format_filetypes = {}
+        '';
+      };
       settings = {
+        log_level = "warn";
+        notify_on_error = false;
+        notify_no_formatters = false;
+        default_format_opts = {
+          timeout_ms = 3000;
+          async = false;
+          quiet = false;
+          lsp_format = "fallback";
+        };
         formatters_by_ft = {
           nix = [ "nixfmt" ];
           sh = [ "shfmt" ];
@@ -49,10 +65,38 @@
             };
           };
         };
-        format_on_save = {
-          lsp_format = "fallback";
-          timeout_ms = 500;
-        };
+        format_on_save = ''
+          function(bufnr)
+            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+              return
+            end
+
+            if slow_format_filetypes[vim.bo[bufnr].filetype] then
+              return
+            end
+
+            local function on_format(err)
+              if err and err:match("timeout$") then
+                slow_format_filetypes[vim.bo[bufnr].filetype] = true
+              end
+            end
+
+            return { timeout_ms = 200, lsp_fallback = true }, on_format
+           end
+        '';
+        format_after_save = ''
+          function(bufnr)
+            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+              return
+            end
+
+            if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+              return
+            end
+
+            return { lsp_fallback = true }
+          end
+        '';
       };
       lazyLoad = {
         enable = true;
@@ -82,15 +126,5 @@
         };
       };
     };
-    extraPackages = with pkgs; [
-      nixfmt
-      shfmt
-      stylua
-      isort
-      black
-      rustfmt
-      prettier
-      prettierd
-    ];
   };
 }
