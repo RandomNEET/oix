04149e0c0f5f01b45260ba860caa31580fd93055
RandomNEET <dev@randomneet.me>
2026-06-16 23:14:41 +0800

    add less optinos

---
+++ modules/base/home/programs/less/default.nix
@@ -0,0 +1,11 @@
+{
+  programs.less = {
+    enable = true;
+    options = {
+      RAW-CONTROL-CHARS = true;
+      ignore-case = true;
+      wheel-lines = 3;
+      mouse = true;
+    };
+  };
+}
