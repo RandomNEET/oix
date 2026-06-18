67e74c5cb4ba8460eeb43f36d97226070ee4ac28
RandomNEET <dev@randomneet.me>
2026-06-18 17:24:25 +0800

    helix: update config

--- modules/optional/home/programs/cli/helix/default.nix
+++ modules/optional/home/programs/cli/helix/default.nix
@@ -2,6 +2,7 @@
   osConfig,
   config,
   lib,
+  pkgs,
   ...
 }:
 {
@@ -10,17 +11,37 @@
     defaultEditor = (config.defaultPrograms.editor == "helix");
     settings = {
       editor = {
-        line-number = "relative";
         default-yank-register = "+";
+
+        line-number = "relative";
+        bufferline = "multiple";
+        popup-border = "menu";
+        color-modes = true;
+
+        soft-wrap = {
+          enable = true;
+        };
       };
       keys = {
         normal = {
           space = {
             space = "file_picker";
+            "$" = "goto_line_end";
+            "0" = "goto_line_start";
           };
         };
       };
     };
+    extraPackages = with pkgs; [
+      bash-language-server # bash
+      clang-tools # c/cpp
+      lua-language-server # lua
+      marksman # markdown
+      nil # nix
+      rust-analyzer # rust
+      vscode-langservers-extracted # html/css/json/eslint
+      yaml-language-server # yaml
+    ];
   };
 
   stylix.targets.helix.enable = lib.mkIf osConfig.desktop.themes.enable true;
