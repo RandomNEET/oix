aec7e89307e7824a3b0f43f043b063b97473af73
RandomNEET <dev@randomneet.me>
2026-06-20 11:31:18 +0800

    ssh: add ControlMaster

--- modules/base/home/programs/ssh/default.nix
+++ modules/base/home/programs/ssh/default.nix
@@ -2,5 +2,12 @@
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
+    settings = {
+      "*" = {
+        ControlMaster = "auto";
+        ControlPath = "/run/user/%i/ssh-%r@%h:%p";
+        ControlPersist = "10m";
+      };
+    };
   };
 }
