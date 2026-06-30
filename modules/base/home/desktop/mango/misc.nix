fe3d84971d44166361a406a7b1f14b0850b3e5c5
RandomNEET <dev@randomneet.me>
2026-06-30 20:04:38 +0800

    mango: add some blur and shadow settings

--- modules/base/home/desktop/mango/misc.nix
+++ modules/base/home/desktop/mango/misc.nix
@@ -4,9 +4,19 @@
   blur_optimized = 1;
   blur_params_radius = 8;
   blur_params_num_passes = 3;
+  blur_params_noise = 0.02;
+  blur_params_brightness = 0.9;
+  blur_params_contrast = 0.9;
+  blur_params_saturation = 1.0;
 
   shadows = 1;
   layer_shadows = 0;
+  shadow_only_floating = 1;
+  shadows_size = 12;
+  shadows_blur = 15;
+  shadows_position_x = 2;
+  shadows_position_y = 2;
+  shadowscolor = "0x000000ff";
 
   borderpx = 2;
   border_radius = 20;
