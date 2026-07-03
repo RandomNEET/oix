2e6138e625077a58a3cf987428e7499bca85c2b7
RandomNEET <dev@randomneet.me>
2026-07-03 22:29:26 +0800

    fzf: fix deprecated option

--- modules/base/home/programs/fzf/default.nix
+++ modules/base/home/programs/fzf/default.nix
@@ -15,25 +15,25 @@
       "--layout=reverse"
       "--border"
     ];
-    historyWidgetOptions = [
+    historyWidget.options = [
       "--sort"
       "--exact"
     ];
   }
   // lib.optionalAttrs config.programs.bat.enable {
-    fileWidgetOptions = [
+    fileWidget.options = [
       "--preview 'bat --color=always --style=numbers --line-range=:500 {}'"
     ];
   }
   // lib.optionalAttrs config.programs.eza.enable {
-    changeDirWidgetOptions = [
+    changeDirWidget.options = [
       "--preview 'eza --tree --color=always {} | head -200'"
     ];
   }
   // lib.optionalAttrs config.programs.fd.enable {
     defaultCommand = "fd --type f --strip-cwd-prefix";
-    fileWidgetCommand = "fd --type f --strip-cwd-prefix";
-    changeDirWidgetCommand = "fd --type d --strip-cwd-prefix";
+    fileWidget.command = "fd --type f --strip-cwd-prefix";
+    changeDirWidget.command = "fd --type d --strip-cwd-prefix";
   };
 
   stylix.targets.fzf.enable = lib.mkIf osConfig.desktop.themes.enable true;
