c74cee61a11cf03d38b12db7454493c7d68bd6be
RandomNEET <dev@randomneet.me>
2026-08-18 11:55:50 +0800

    opencode: add plugin oh-my-opencode-slim

--- modules/optional/home/programs/cli/opencode/default.nix
+++ modules/optional/home/programs/cli/opencode/default.nix
@@ -11,16 +11,31 @@
     settings = {
       autoshare = false;
       autoupdate = true;
+      agent = {
+        explore.disable = true;
+        general.disable = true;
+      };
+      lsp = true;
+      plugin = [ "oh-my-opencode-slim" ];
     };
     tui = {
       keybinds = {
         leader = "ctrl+x";
       };
+      plugin = [ "oh-my-opencode-slim" ];
     };
     extraPackages = with pkgs; [
+      bun
       nodejs
       uv
     ];
+  };
+  home = {
+    file.".config/opencode/oh-my-opencode-slim.jsonc".source = ./plugins/oh-my-opencode-slim.jsonc;
+    sessionVariables = {
+      OPENCODE_EXPERIMENTAL_BACKGROUND_SUBAGENTS = "true";
+      OPENCODE_ENABLE_EXA = "1";
+    };
   };
 }
 // lib.optionalAttrs osConfig.desktop.themes.enable {
