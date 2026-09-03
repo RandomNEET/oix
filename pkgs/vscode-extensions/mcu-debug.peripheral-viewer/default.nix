fdd23f063df05981045fd11927ba242faee8052c
RandomNEET <dev@randomneet.me>
2026-09-03 19:33:48 +0800

    vscode-extensions.peripheral-viewer: 1.6.1 -> 1.6.3

--- pkgs/vscode-extensions/mcu-debug.peripheral-viewer/default.nix
+++ pkgs/vscode-extensions/mcu-debug.peripheral-viewer/default.nix
@@ -3,8 +3,8 @@ pkgs.vscode-utils.buildVscodeMarketplaceExtension {
   mktplcRef = {
     publisher = "mcu-debug";
     name = "peripheral-viewer";
-    version = "1.6.1";
-    hash = "sha256-DwaL0lct8KevC7AVFLydQTQEr1mC1Rz+P+jl+zHoN+k=";
+    version = "1.6.3";
+    hash = "sha256-ElqS0Zsd1qptgQyQI1lAVwyK8oDJYuoF6Q8UkMBnqNM=";
   };
 
   meta = with lib; {
